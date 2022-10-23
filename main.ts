badc7c98fa3b6c8279cd68d068f4ce4c1dfaa662
MrPigeontheCoder <105377999+MrPigeontheCoder@users.noreply.github.com>
2022-10-23 12:58:51 +0200

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -8,28 +8,35 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 // This sets up the game and stuff
 let Player1: Sprite = null
+console.log("game started")
 // This creates the player sprite and makes it move and makes the camera follow it
 Player1 = sprites.create(assets.image`Player1Right`, SpriteKind.Player)
 controller.moveSprite(Player1, 50, 0)
 scene.cameraFollowSprite(Player1)
-let lives = 5
+info.setLife(5)
+console.log("player created, lives set")
+console.logValue("lives", 5)
 // This sets the level1 and also sets up the sky
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.setBackgroundImage(assets.image`Sky`)
 // This places the player at the start of the level
 tiles.placeOnTile(Player1, tiles.getTileLocation(0, 11))
+console.log("player placed on position")
 // This shows the name of the game and copyright and stuff
 // 
 game.splash("SUPER AMOGUS BROS.", "Copyright Amogus Co. 2022")
+console.log("name splashed")
 // This shows the lives
-game.showLongText("" + lives + " x Lives", DialogLayout.Full)
+game.showLongText("" + info.life() + " x Lives", DialogLayout.Full)
+console.log("lives shown")
 let Moving_Enemy = sprites.create(assets.image`enemy1left`, SpriteKind.Enemy)
 tiles.placeOnTile(Moving_Enemy, tiles.getTileLocation(3, 12))
 Moving_Enemy.setVelocity(50, 0)
 Moving_Enemy.setBounceOnWall(true)
+console.log("enemy set")
 forever(function () {
     if (!(controller.A.isPressed())) {
-        Player1.vy = 50
+        Player1.vy = 75
     }
 })
 forever(function () {
